0d40cfacf6a574b0b4fee95257315af3df1a2134
= <=Turqay@mail.ru>
2024-04-01 03:14:15 +0400

    Add project files.

---
+++ TCPListener_Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+public class Client
+{
+    private const string SERVER_IP = "127.0.0.1";
+    private const int PORT = 8080;
+
+    static void Main(string[] args)
+    {
+        Console.Write("Adi daxil edin: ");
+        string userName = Console.ReadLine();
+
+        TcpClient client = new TcpClient(SERVER_IP, PORT);
+        NetworkStream stream = client.GetStream();
+
+        byte[] userNameBytes = Encoding.ASCII.GetBytes(userName);
+        stream.Write(userNameBytes, 0, userNameBytes.Length);
+
+        Console.WriteLine("Servera  isledi ");
+
+        while (true)
+        {
+            Console.Write("Kime mesaj yazmaq isteyirsen ID  (0 dan basla ): ");
+            int recipientID = int.Parse(Console.ReadLine());
+
+            Console.Write("mesaj yaz ... : ");
+            string message = Console.ReadLine();
+
+            byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+            stream.Write(messageBytes, 0, messageBytes.Length);
+        }
+    }
+}
---
+++ TCPListener_task/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class Server
+{
+    private static Dictionary<int, string> users = new Dictionary<int, string>();
+    private static TcpListener listener;
+    private const int PORT = 8080;
+    private static List<NetworkStream> clientStreams = new List<NetworkStream>();
+
+    static void Main(string[] args)
+    {
+        listener = new TcpListener(IPAddress.Any, PORT);
+        listener.Start();
+        Console.WriteLine("Server running...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClient));
+            clientThread.Start(client);
+        }
+    }
+
+    static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+        string userName = "";
+
+        try
+        {
+            // Prompt user for their name
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            userName = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            users[Thread.CurrentThread.ManagedThreadId] = userName;
+            Console.WriteLine(userName + " connected.");
+            clientStreams.Add(stream); // Add new client's stream for broadcasting
+
+            while (true)
+            {
+                // Read message from client
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(userName + ": " + message);
+
+                // Broadcast message to other clients
+                byte[] messageBytes = Encoding.ASCII.GetBytes(userName + ": " + message);
+                foreach (var clientStream in clientStreams)
+                {
+                    clientStream.Write(messageBytes, 0, messageBytes.Length);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(userName + " disconnected.");
+            users.Remove(Thread.CurrentThread.ManagedThreadId);
+            clientStreams.Remove(stream); // Remove stream when user leaves
+        }
+        finally
+        {
+            stream.Close();
+            client.Close();
+        }
+    }
+}
